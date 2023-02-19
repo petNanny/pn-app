e83e334a8162dd46acd7b41592fb61332abab484
Dylan Lang <fullartonstory@gmail.com>
2023-02-19 13:03:31 +1030

    Create Navbar.test.tsx

---
+++ src/__test__/Navbar.test.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "../components/Navbar";
+import { render, screen } from "@testing-library/react";
+
+it("renders without crashing", () => {});
