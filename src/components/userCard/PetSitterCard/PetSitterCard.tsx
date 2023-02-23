78359c6349b925fc5d753c314dc9fff747b1b6ae
joeZheng <joezheng416@gmail.com>
2023-02-23 22:29:16 +1100

    feat: first commit

--- src/components/userCard/PetSitterCard/PetSitterCard.tsx
+++ src/components/userCard/PetSitterCard/PetSitterCard.tsx
@@ -22,36 +22,37 @@ const PetSitterCard = (prop: PetSitter) => {
             <Avatar size="lg" name={prop.name} src={prop.avatar} />
             <Box>
               <Flex flexDirection="column" alignItems="flex-start">
-                <Heading size='sm'>{prop.name}</Heading>
-                <Text opacity="0.5">{prop.distance}km - {prop.suburb} </Text>
+                <Heading size="sm">{prop.name}</Heading>
+                <Text opacity="0.5">
+                  {prop.distance}km - {prop.suburb}{" "}
+                </Text>
                 <div>
-                  {Array(5).fill('').map((_, i) => (
-                                            <StarIcon
-                                                key={i}
-                                                color={i < prop.rating ? 'yellow.500' : 'gray.300'}
-                                            />
-                                        ))}
-                  </div>
+                  {Array(5)
+                    .fill("")
+                    .map((_, i) => (
+                      <StarIcon key={i} color={i < prop.rating ? "yellow.500" : "gray.300"} />
+                    ))}
+                </div>
               </Flex>
             </Box>
-            </Flex>
-                    <Box>
-                        <Text opacity="0.8">{prop.price} AUD <br></br>/ night </Text>
-                    </Box>
-                </Flex>
-            </CardHeader>
-            <CardBody>
-                <Text className="Card-introduction">
-                    {prop.introduction}
-                </Text>
-            </CardBody>
-            <CardFooter>
-                <Text width="100%" textAlign="right" color="blue.500">Read more</Text>
-
-            </CardFooter>
-        </Card>
-    )
-
-}
+          </Flex>
+          <Box>
+            <Text opacity="0.8">
+              {prop.price} AUD <br></br>/ night{" "}
+            </Text>
+          </Box>
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <Text className="Card-introduction">{prop.introduction}</Text>
+      </CardBody>
+      <CardFooter>
+        <Text width="100%" textAlign="right" color="blue.500">
+          Read more
+        </Text>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default PetSitterCard;
