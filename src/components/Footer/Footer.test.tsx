ed29d65abc6a74c2e0f09be9a8722a9de7e265ad
Thomas Yan <ie0481@hotmail.com>
2023-02-26 11:44:27 +1030

    add footer test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,1 +1,22 @@
-export {}
+import { render, screen } from "@testing-library/react";
+import Footer from "./Footer";
+
+describe("Footer", () => {
+  it("renders the component correctly", () => {
+    render(<Footer />);
+    const learnMore = screen.getByText("Learn More");
+    const popularCities = screen.getByText("Popular Cities");
+    const aboutUs = screen.getByText("About Us");
+    const findUsOn = screen.getByText("Find us on");
+
+    expect(learnMore).toBeInTheDocument();
+    expect(popularCities).toBeInTheDocument();
+    expect(aboutUs).toBeInTheDocument();
+    expect(findUsOn).toBeInTheDocument();
+  });
+});
+
+
+
+
+
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,160 +1,171 @@
-import { Button } from "@chakra-ui/react";
-import "./Footer.css";
+import theme from "../../styles/theme";
+import visa from "./img/visa.svg";
+import mastercard from "./img/mastercard.svg";
+import americanExpress from "./img/americanExpress.svg";
+import { Box, Text, Link, Image, UnorderedList, Heading, Select } from "@chakra-ui/react";
+import {
+  StyledFooterBox,
+  StyledFooterFlex,
+  StyledSelectBox,
+  StyledListItem,
+  StyledImageBox,
+  StyledLink,
+  StyledListBox,
+  StyledFooterTopFlex,
+  StyledFooterBottomFlex,
+} from "./StyledFooter";
+const primary = theme.colors.background.primary;
+const quaternary = theme.colors.background.quaternary;
+
 const Footer = () => {
   return (
-    <div className="footerContainer">
-      <div className="footerContainerTop">
-        <div className="information">
-          <div className="learnMore">
-            <h2>Learn More</h2>
-            <ul>
-              <li>
-                <a>How does Pawshake work?</a>
-              </li>
-              <li>
-                <a>Testimonials</a>
-              </li>
-              <li>
-                <a>The Pawshake Guarantee</a>
-              </li>
-              <li>
-                <a>Payments and refunds</a>
-              </li>
-              <li>
-                <a>FAQ for Pet Owners</a>
-              </li>
-              <li>
-                <a>FAQ for Pet Sitters</a>
-              </li>
-              <li>
-                <a>Dog boarding</a>
-              </li>
-              <li>
-                <a>Doggy day care</a>
-              </li>
-              <li>
-                <a>Dog walking</a>
-              </li>
-              <li>
-                <a>Cat sitting</a>
-              </li>
-              <li>
-                <a>Dog sitting</a>
-              </li>
-              <li>
-                <a>Pet sitting</a>
-              </li>
-              <li>
-                <a>House sitting</a>
-              </li>
-            </ul>
-          </div>
-          <div className="popularCities">
-            <h2>Popular Cities</h2>
-            <ul>
-              <li>
-                <a>Melbourne</a>
-              </li>
-              <li>
-                <a>Sydney</a>
-              </li>
-              <li>
-                <a>Brisbane</a>
-              </li>
-              <li>
-                <a>Perth</a>
-              </li>
-              <li>
-                <a>Adelaide</a>
-              </li>
-              <li>
-                <a>Gold Coast</a>
-              </li>
-              <li>
-                <a>Sunshine Coast</a>
-              </li>
-              <li>
-                <a>Penrith</a>
-              </li>
-              <li>
-                <a>Tweed Heads</a>
-              </li>
-              <li>
-                <a>Mornington</a>
-              </li>
-            </ul>
-            <div className="topCities">
-              <a href="">Top doggy day care cities</a>
-            </div>
-          </div>
-          <div className="about">
-            <h2>About Us</h2>
-            <ul>
-              <li>
-                <a>Our Story</a>
-              </li>
-              <li>
-                <a>In the press</a>
-              </li>
-              <li>
-                <a>Blog</a>
-              </li>
-              <li>
-                <a>Help</a>
-              </li>
-            </ul>
-            <h2 className="payment">Payment Methods</h2>
-            <div className="img">
-              <div>
-                <img src="	https://static1.pawshakecdn.com/payment-methods/visa.svg" alt="visa" />
-              </div>
-              <div>
-                <img
-                  src="https://static1.pawshakecdn.com/payment-methods/mastercard.svg"
-                  alt="mastercard"
-                />
-              </div>
-              <div>
-                <img
-                  src="https://static1.pawshakecdn.com/payment-methods/americanExpress.svg"
-                  alt="americanExpress"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="findUsOn">
-            <h2>Find us on</h2>
-            <ul>
-              <li>
-                <a href="https://www.facebook.com/">Facebook</a>
-              </li>
-              <li>
-                <a href="https://twitter.com/">Twitter</a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/">Instagram</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="footerContainerBottom">
-        <div className="termAndCountry">
-          <div className="terms">
-            <p>
-              Copyright © 2023 Pet-Nany Inc. All Rights Reserved. <a>Terms and Conditions</a> and
-              <a>Privacy & Cookie Policy</a>.
-            </p>
-          </div>
-          <select className="country">
-            <option value="Australia(EN)">Australia(EN)</option>
-            <option value="Canada(EN)">Canada(EN)</option>
-            <option value="HongKong(CN)">HongKong(CN)</option>
-            <option value="Amercia(EN)">Amercia(EN)</option>
-          </select>
-        </div>
-      </div>
-    </div>
+    <StyledFooterBox>
+      <StyledFooterFlex bg={primary}>
+        <StyledFooterTopFlex>
+          <StyledListBox>
+            <Heading>Learn More</Heading>
+            <UnorderedList>
+              <StyledListItem>
+                <Link>How does Pawshake work?</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Testimonials</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>The Pawshake Guarantee</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Payments and refunds</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>FAQ for Pet Owners</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>FAQ for Pet Sitters</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Dog boarding</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Doggy day care</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Dog walking</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Cat sitting</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Dog sitting</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Pet sitting</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>House sitting</Link>
+              </StyledListItem>
+            </UnorderedList>
+          </StyledListBox>
+          <StyledListBox>
+            <Heading>Popular Cities</Heading>
+            <UnorderedList>
+              <StyledListItem>
+                <Link>Melbourne</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Sydney</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Brisbane</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Perth</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Adelaide</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Gold Coast</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Sunshine Coast</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Penrith</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Tweed Heads</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Mornington</Link>
+              </StyledListItem>
+            </UnorderedList>
+            <Box pl="1rem">
+              <Link href="">Top doggy day care cities</Link>
+            </Box>
+          </StyledListBox>
+          <StyledListBox>
+            <Heading>About Us</Heading>
+            <UnorderedList>
+              <StyledListItem>
+                <Link>Our Story</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>In the press</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Blog</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link>Help</Link>
+              </StyledListItem>
+            </UnorderedList>
+            <Heading>Payment Methods</Heading>
+            <StyledImageBox>
+              <Image src={visa} alt="visa" />
+
+              <Image src={mastercard} alt="mastercard" />
+
+              <Image src={americanExpress} alt="americanExpress" />
+            </StyledImageBox>
+          </StyledListBox>
+          <StyledListBox>
+            <Heading>Find us on</Heading>
+            <UnorderedList>
+              <StyledListItem>
+                <Link href="https://www.facebook.com/">Facebook</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link href="https://twitter.com/">Twitter</Link>
+              </StyledListItem>
+              <StyledListItem>
+                <Link href="https://www.instagram.com/">Instagram</Link>
+              </StyledListItem>
+            </UnorderedList>
+          </StyledListBox>
+        </StyledFooterTopFlex>
+      </StyledFooterFlex>
+
+      <StyledFooterFlex bg={quaternary}>
+        <StyledFooterBottomFlex>
+          <Box>
+            <Text>
+              Copyright © 2023 Pet-Nany Inc. All Rights Reserved.{" "}
+              <StyledLink>Terms and Conditions</StyledLink> and{" "}
+              <StyledLink>Privacy & Cookie Policy</StyledLink>.
+            </Text>
+          </Box>
+          <StyledSelectBox>
+            <Select iconColor="rgb(116, 116, 116)">
+              <option value="Australia(EN)">Australia(EN)</option>
+              <option value="Canada(EN)">Canada(EN)</option>
+              <option value="HongKong(CN)">HongKong(CN)</option>
+              <option value="Amercia(EN)">Amercia(EN)</option>
+            </Select>
+          </StyledSelectBox>
+        </StyledFooterBottomFlex>
+      </StyledFooterFlex>
+    </StyledFooterBox>
   );
 };
 
