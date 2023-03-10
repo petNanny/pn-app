c4fcd31183c9536cf169769e038574f204bd59bb
Tong Lin <wowawawoa@gmail.com>
2023-03-10 12:22:08 +1030

    feat: add swpie in gallery

---
+++ src/components/PetSitterPagePetSitterProfile/components/PetSitterGallery/PetSitterGallery.tsx
@@ -0,0 +1,81 @@
+import { Box, Image } from "@chakra-ui/react";
+import imageNotFound from "../../../../assets/temps/imageNotFound.jpg";
+import {
+  PetSitterGalleryContainer,
+  PetSitterGalleryImageContainer,
+  CoverImage,
+  BackImage,
+  MoreImagesBtn,
+  ImageTitle,
+} from "./styledPetSitterGallery";
+import { useGetPetSitterImagesQuery } from "../../../../redux/api/attachmentApi";
+import { useEffect, useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { extractIdFromURL } from "../../../../utils/common";
+import { useLocation } from "react-router-dom";
+
+interface Image {
+  _id: string;
+  url: string;
+  fileName: string;
+  petSitterId: string;
+}
+
+const PetSitterGallery = () => {
+  const defaultImage = {
+    _id: "1",
+    url: imageNotFound,
+    fileName: "image not found",
+    petSitterId: "0",
+  };
+  const [getImages, setGetImages] = useState<Image[]>([defaultImage]);
+  const [imageGalleryOpen, setImageGalleryOpen] = useState(false);
+
+  const location = useLocation();
+  const petSitterId = extractIdFromURL(location.pathname);
+
+  const { data: petSitterData, isLoading: isProfileImageLoading } =
+    useGetPetSitterImagesQuery(petSitterId);
+
+  useEffect(() => {
+    if (petSitterData !== undefined && petSitterData.images !== undefined) {
+      setGetImages(petSitterData.images);
+    }
+  }, [petSitterData]);
+
+  const slides = getImages.map((image) => {
+    return { ...image, src: image.url, url: undefined };
+  });
+
+  const handleMoreImgBtn = () => {
+    setImageGalleryOpen(true);
+  };
+
+  const handleImageGalleryCloseBtn = () => {
+    setImageGalleryOpen(false);
+  };
+
+  return (
+    <>
+      <PetSitterGalleryContainer>
+        <PetSitterGalleryImageContainer>
+          <Box>
+            <CoverImage src={getImages[0].url} alt={getImages[0].fileName} />
+            <BackImage src={getImages[0].url} alt={getImages[0].fileName} />
+          </Box>
+          <MoreImagesBtn onClick={handleMoreImgBtn}>See more ({getImages.length})</MoreImagesBtn>
+          <Lightbox
+            open={imageGalleryOpen}
+            close={handleImageGalleryCloseBtn}
+            slides={slides}
+            carousel={{ imageFit: "contain" }}
+          />
+        </PetSitterGalleryImageContainer>
+        <ImageTitle as="h1">Tong</ImageTitle>
+      </PetSitterGalleryContainer>
+    </>
+  );
+};
+
+export default PetSitterGallery;
---
+++ src/components/PetSitterPagePetSitterProfile/components/PetSitterGallery/styledPetSitterGallery.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { Button, Box, Image, Heading } from "@chakra-ui/react";
+
+export const PetSitterGalleryContainer = styled(Box)`
+  &&& {
+    position: relative;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const PetSitterGalleryImageContainer = styled(Box)`
+  &&& {
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+`;
+
+export const CoverImage = styled(Image)`
+  &&& {
+    height: 26rem;
+    max-width: 100%;
+    margin: auto;
+  }
+`;
+
+export const BackImage = styled(Image)`
+  &&& {
+    height: 26rem;
+    position: absolute;
+    width: 100%;
+    top: 0;
+    z-index: -1;
+    filter: blur(50px);
+  }
+`;
+
+export const MoreImagesBtn = styled(Button)`
+  &&& {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    width: 20%;
+    border-radius: 4px;
+    touch-action: manipulation;
+    cursor: pointer;
+    background-color: white;
+    border: 1px solid rgb(206, 206, 206);
+    color: rgb(79, 79, 79);
+    font-weight: normal;
+  }
+`;
+
+export const ImageTitle = styled(Heading)`
+  &&& {
+    position: absolute;
+    left: 1rem;
+    bottom: 1rem;
+    color: rgb(255, 255, 255);
+    margin: 0;
+    line-height: 2rem;
+    font-size: 2rem;
+  }
+`;
