44796790245a29c067fb14d64e9bba16d616a296
Dylan Lang <fullartonstory@gmail.com>
2023-03-22 10:29:40 +1030

    Add new ErrorImage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,16 +2,13 @@ import { Heading, Text } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 import PageLayout from "../../components/Layout/PageLayout";
 import { ErrorPageContent } from "./styledErrorPage";
+import ErrorImage from "./assets/3.jpg";
 
 const ErrorPage = () => {
   return (
     <PageLayout>
       <ErrorPageContent>
-        <img
-          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2VmNzc2MTM0ZjIxNjdhM2JhNzRmMWEwMzY4MDMzNThhNjVkYzFiNiZjdD1n/AKWXpDjlLgYFe1cZou/giphy.gif"
-          id="error-logo"
-          alt="PetNanny 404 Page"
-        />
+        <img src={ErrorImage} id="error-logo" alt="PetNanny 404 Page" />
         <Heading color="#5CACE2">
           Oops... something went wrong. we are working to
           <Heading color="#ADD8E6">
