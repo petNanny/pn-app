f708a9f51ca924e54a6969e9a96292958a7d6c8d
Tong Lin <wowawawoa@gmail.com>
2023-04-02 22:54:07 +0930

    feat: add pet sitter skills on pet sitter page

--- src/components/PetSitterDetail/styledPetSitterDetail.ts
+++ src/components/PetSitterDetail/styledPetSitterDetail.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import { devices } from "../../styles/breakPoints";
 
 export const PetSitterPageContainer = styled(Box)`
@@ -17,6 +17,28 @@ export const ProfileContentContainer = styled(Box)`
     width: 100%;
     @media ${devices.laptop} {
       width: 65%;
+    }
+  }
+`;
+
+export const ProfileDetailSection = styled(Box)`
+  &&& {
+    margin-bottom: 2rem;
+    @media ${devices.laptop} {
+      margin-bottom: 4rem;
+    }
+  }
+`;
+
+export const ProfileDetailSectionHeading = styled(Heading)`
+  &&& {
+    font-weight: 400;
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    line-height: 40px;
+    color: rgb(79, 79, 79);
+    @media ${devices.tablet} {
+      font-size: 2rem;
     }
   }
 `;
