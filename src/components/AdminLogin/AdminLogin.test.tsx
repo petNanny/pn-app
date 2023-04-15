d94868e59df66229cfb6fa86d091be570c320b3c
Dylan Lang <fullartonstory@gmail.com>
2023-04-15 21:14:57 +0930

    test
    adding missing tests, and refactoring tests.

--- src/components/AdminLogin/AdminLogin.test.tsx
+++ src/components/AdminLogin/AdminLogin.test.tsx
@@ -1,1 +1,13 @@
 import AdminLogin from "./AdminLogin";
+import renderWithMockedProvider from "../../__test__/utils/renderWithMockedProvider";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("login component", () => {
+  it("should render the login component correctly", () => {
+    renderWithMockedProvider(<AdminLogin />);
+    expect(screen.queryByPlaceholderText("Email")).toBeInTheDocument();
+    expect(screen.queryByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /login/i })).toBeInTheDocument();
+  });
+});
